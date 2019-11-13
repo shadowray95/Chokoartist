6521de8967fe39d7c742d00f6980a34d5a7153d8
shadowray95 <meet.navy07@gmail.com>
2019-11-14 00:15:27 +0530

    First Commit

---
+++ app/src/main/java/com/shadowraylabs/chokoartist/MainActivity.java
@@ -0,0 +1,38 @@
+package com.shadowraylabs.chokoartist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login, register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.main_login_btn);
+        register = (Button) findViewById(R.id.main_register_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
